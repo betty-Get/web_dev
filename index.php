c512ed561f41cf48ff8e7dfe846520eeb5b9b036
betty-Get <betelhem.getachew2926@gmail.com>
2022-09-05 05:45:01 -0700

    new commit

--- index.php
+++ index.php
@@ -3,13 +3,37 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <link rel="stylesheet" href="main.css">
   </head>
-  <body>
-    <h1>home page</h1>
-    <a href="signup.php">sign in</a>
-    
-  
-</form>
-  </body>
+
+ 
+
+<body>
+<div class="container">
+	
+    <div class="background">
+        <div class="shape"></div>
+        <div class="shape"></div>
+    </div>
+    <form>
+        <h3>Login Here</h3>
+
+        <label for="username">Username</label>
+        <input type="text" placeholder="Email Address" id="username">
+
+        <label for="password">Password</label>
+        <input type="password" placeholder="Password" id="password">
+
+        <button>Log In</button>
+        <div class="">
+            <p small>if you dont have an account</p>
+            <a href="">Sign Up</a>
+        </div>
+        <div class="">
+            <p small></p>
+            <a href="signup.php">Sign Up</a>
+        </div>
+    </form>
+</div>
+</body>
 </html>
--- signup.php
+++ signup.php
@@ -1,19 +1,47 @@
+<?php
+require_once('config.php');
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <link rel="stylesheet" href="main.css">
   </head>
-  <body>
+
+ 
+
+<body>
+<div class="container">
+
+    <form>
+        <h3>Sign Up Here</h3>
+        
     
-    <form action="/action_page.php">
-  <label for="fname">First name:</label><br>
-  <input type="text" id="fname" name="fname" value="John"><br>
-  <label for="lname">Last name:</label><br>
-  <input type="text" id="lname" name="lname" value="Doe"><br><br>
-  <input type="submit" value="Submit">
-  
-</form>
-  </body>
+        <input type="text" placeholder="FirstName" id="FirstName">
+
+       
+        <input type="LastName" placeholder="LastName" id="LastName">
+
+        
+        <input type="Gender" placeholder="Gender" id="Gender">
+        
+        <input type="Email" placeholder="Email" id="Eamil">
+        
+        <input type="DateOfBirth" placeholder="DateOfBirth" id="DateOfBirth">
+
+
+        <input type="Phone number" placeholder="Phone number" id="Phone number">
+
+        
+        <input type="Address" placeholder="Address" id="Address">
+
+        <button>Log In</button>
+        
+    </form>
+</div>
+</body>
 </html>
+
+
